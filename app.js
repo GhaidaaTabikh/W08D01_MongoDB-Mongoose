4779f89187df07a3f2ef5c10c889144d305afecb
Ghaidaa_tabikh <tabikh.ghaidaa@gmail.com>
2021-05-17 18:53:48 +0300

    practice 1+2+3+4 done

--- app.js
+++ app.js
@@ -5,9 +5,64 @@ const db = require("./db");
 const app = express();
 app.use(express.json());
 
-app.get("/todos", (req, res) => {});
-app.post("/create/todo", (req, res) => {});
-app.put("/update/todo", (req, res) => {});
+
+
+//qui1 practice
+
+app.post("/create/todo", (req, res) => {
+  const { task, description, deadline, isCompleted, priority } = req.body;
+  const todoList = new todoModel({
+    task,
+    description,
+    deadline,
+    isCompleted,
+    priority,
+  });
+  todoList
+    .save()
+    .then((result) => {
+      res.json(result);
+    })
+    .catch((err) => {
+      console.log("ghaidaaerror");
+      res.send(err);
+    });
+});
+
+// qui3 practice
+app.get("/todos", (req, res) => {
+  todoModel
+  .find({})
+  .then((result) => {
+    res.send(result);
+  })
+  .catch((err) => {
+    res.send(err);
+  });
+});
+
+//qui4 practice
+app.get("/todos", (req, res) => {
+  todoModel
+  .find({"isCompleted":true})
+  .then((result) => {
+    res.send(result);
+  })
+  .catch((err) => {
+    res.send(err);
+  });
+});
+
+//qui5 practice
+app.put("/update/todo", async (req, res) => {
+  const { task, description, deadline, isCompleted, priority } = req.body
+  const updateTodo = await todo.findOne({task, description, deadline, isCompleted, priority })
+  
+  try{
+    await mongoose.model('todo').update({ task: 'Ned Stark' }).j(true);}
+  catch{res.json("new error")}
+  
+});
 app.delete("/delete/todo", (req, res) => {});
 
 const port = 3000;
--- schema.js
+++ schema.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const { stringify } = require("querystring");
+
 
 //que3
 
